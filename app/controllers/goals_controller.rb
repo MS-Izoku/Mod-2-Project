ab0d157f4abff1f1d0336db2a5867d57bf44546c
Philip Roth <50246817+rothberry@users.noreply.github.com>
2019-07-30 16:33:23 -0500

    adding goal&usergoal controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,47 @@
 class GoalsController < ApplicationController
+    def index
+        @goals = Goal.all
+    end
+    def show
+        @goal = Goal.find(params[:id])
+    end
+    
+    def new
+        @goal = Goal.new
+    end
+    def create
+        @goal = Goal.new(params[:goal])
+        if @goal.save
+          flash[:success] = "Goal successfully created"
+          redirect_to @goal
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+    def edit
+        @goal = Goal.find(params[:id])
+    end
+    def update
+        @goal = Goal.find(params[:id])
+        if @goal.update_attributes(params[:goal])
+          flash[:success] = "Goal was successfully updated"
+          redirect_to @goal
+        else
+          flash[:error] = "Something went wrong"
+          render 'edit'
+        end
+    end
+    def destroy
+        @goal = Goal.find(params[:id])
+        if @goal.destroy
+            flash[:success] = 'Goal was successfully deleted.'
+            redirect_to goals_url
+        else
+            flash[:error] = 'Something went wrong'
+            redirect_to goals_url
+        end
+    end
+    
+    
 end
--- app/controllers/user_goals_controller.rb
+++ app/controllers/user_goals_controller.rb
@@ -1,2 +1,21 @@
 class UserGoalsController < ApplicationController
+    def new
+        @usergoal = UserGoal.new
+    end
+
+    def show
+        @usergoal = UserGoal.find(params[:id])
+    end
+    
+    def edit
+        @usergoal = UserGoal.find(params[:id])
+    end 
+
+
+
+    private
+
+    def usergoal_params
+        params.require(:usergoal).permit(:user_id, :goal_id, :frequency, :completion, :notes) 
+    end
 end
