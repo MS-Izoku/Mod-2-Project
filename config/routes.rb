a20693f1f792dd6af62ca01265c25433af374eb0
github username <github email address>
2019-07-30 15:56:10 -0500

    Fixed a typo

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   get '/login' , to: 'sessions#new' , as: 'login'
   post '/login' , to: 'sessions#create'
-  delete '/logout' , to: 'sessions#destory'
+  delete '/logout' , to: 'sessions#destroy'
+
+  get '/' , to: 'user_goals#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
