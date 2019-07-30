71cc368be7b2ddb803072f71cfcb8fcb52c5e385
github username <github email address>
2019-07-30 16:26:03 -0500

    mvc boiler plate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,59 @@
 class UsersController < ApplicationController
-end
+    before_action :find_user, only: [:edit, :update, :show, :destroy, :index] 
+
+    def new
+        @user = User.new 
+    end 
+
+    def index 
+        @users = User.all
+    end 
+
+
+    def create 
+        @user = User.new(user_params)
+        if @user.valid?
+           @user.save 
+            redirect_to user_path(@user)
+        else 
+            render :new
+        end
+    end 
+
+    def show 
+    end 
+
+    def edit 
+    end 
+
+    
+    def update
+        @user.assign_attributes(user_params)
+
+            if @user.save
+              redirect_to @user
+            else
+              render :new
+            end
+          end
+         
+
+    def destroy
+        @user.destroy 
+        flash[:notice] = "Account Deleted. Sorry to see you go"
+        # need a path to a home page of some sort. 
+    end 
+
+
+    private
+    
+    def find_user
+        @user = User.find_by(id: params[:id])
+    end
+
+
+    def user_params
+        params.require(:user).permit(:username, :password, :password_confirmation, :full_name, :city, :programming_language, :img_url)
+    end 
+end 
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
     has_many :user_goals
     #has_many :progress_updates , through: :user_goals
     has_many :goals , through: :user_goals
+    has_secure_password 
 
     def like_count
         self.likes.count
@@ -13,3 +14,4 @@ class User < ApplicationRecord
         self.comments.count
     end
 end
+
