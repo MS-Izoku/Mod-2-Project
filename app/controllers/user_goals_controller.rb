440258e974ceb4ae5f438a6339440abdded35eb7
Philip Roth <50246817+rothberry@users.noreply.github.com>
2019-07-31 13:31:30 -0500

    add render partial

--- app/controllers/user_goals_controller.rb
+++ app/controllers/user_goals_controller.rb
@@ -1,19 +1,42 @@
 class UserGoalsController < ApplicationController
+    before_action :find_user_goal, only: [:edit, :update, :show, :destory]
+    
     def new
         @usergoal = UserGoal.new
     end
 
+    def create
+        @usergoal = UserGoal.new(params[:usergoal])
+        if @usergoal.save
+          flash[:success] = "UserGoal successfully created"
+          redirect_to @usergoal
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+    
     def show
-        @usergoal = UserGoal.find(params[:id])
     end
     
     def edit
-        @usergoal = UserGoal.find(params[:id])
     end 
 
-
-
+    def update
+        if @usergoal.update_attributes(params[:usergoal])
+          flash[:success] = "UserGoal was successfully updated"
+          redirect_to @usergoal
+        else
+          flash[:error] = "Something went wrong"
+          render 'edit'
+        end
+    end
+    
     private
+
+    def find_user_goal
+        @usergoal = UserGoal.find(params[:id])
+    end
 
     def usergoal_params
         params.require(:usergoal).permit(:user_id, :goal_id, :frequency, :completion, :notes) 
