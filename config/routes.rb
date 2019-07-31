f5a31812914e21e3661001f511c32d362c7d6d03
github username <github email address>
2019-07-31 11:46:55 -0500

    Changed routing and added in flash/helper methods

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   post '/login' , to: 'sessions#create'
   delete '/logout' , to: 'sessions#destroy'
 
-  get '/' , to: 'user_goals#new'
+  root to: 'progress_updates#index'
+  #get '/' , to: 'user_goals#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
