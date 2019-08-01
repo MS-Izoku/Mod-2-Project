3715dbcbed1cb86b364a7943ab0b4235213f587e
github username <github email address>
2019-08-01 16:52:19 -0500

    some html styling and forms rearranging

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
+UserGoal.destroy_all
+ProgressUpdate.destroy_all
+Comment.destroy_all
+Like.destroy_all
+
+
 User.create(username: "Admin" , full_name: "Admin2" , password: "password")
 5.times do
     User.create(username: Faker::Name.name , full_name: Faker::Name.name , city: "Chicago" , programming_language: "Ruby" , password: "password")
 end
 
-Goal.create(name: "I am a Goal")
-Goal.create(name: "Sleep, damn it")
-Goal.create(name: "Eat a single carrot")
-Goal.create(name: "Test Title!")
-Goal.create(name: "Try Yodeling")
+Goal.create(name: "Eating")
+Goal.create(name: "Sleeping")
+Goal.create(name: "Coding")
+Goal.create(name: "Eating a single carrot")
+Goal.create(name: "Yodeling")
+Goal.create(name: "Exercising")
+Goal.create(name: "Drinking Water")
+
 
 # 5.times do
 #     UserGoal.create(user_id: User.all.shuffle[0].id , goal_id: Goal.all.shuffle[0].id)
@@ -24,4 +34,6 @@ Goal.create(name: "Try Yodeling")
 # 5.times do
 #     ProgressUpdate.create(content: Faker::Lorem.paragraph(2 , false) , user_goal_id: UserGoal.all.shuffle[0].id)
 # end
+
+
 
