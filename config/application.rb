78bfae812e9bb23602b6a5b7c95b30a326703abd
github username <github email address>
2019-08-01 21:47:03 -0500

    Added form-styling to sign-up and linked the form-folder to the application pipeline in application.rb

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,9 @@ module Mod2Final
     html_tag
     }
 
+    # adding the fonts folder to the asset pipeline -ms
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
